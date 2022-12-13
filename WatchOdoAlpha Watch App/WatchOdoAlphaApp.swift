a8191d62f7f6a054a661a41f6b0f8c4c2a214c51
Dan Payne <dpayne5532@gmail.com>
2022-12-13 10:54:06 -0700

    intiial commit

---
+++ WatchOdoAlpha Watch App/WatchOdoAlphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOdoAlphaApp.swift
+//  WatchOdoAlpha Watch App
+//
+//  Created by Dan Payne on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchOdoAlpha_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchOdoAlpha/WatchOdoAlphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOdoAlphaApp.swift
+//  WatchOdoAlpha
+//
+//  Created by Dan Payne on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchOdoAlphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
